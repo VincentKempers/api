5075de31e327e7b758a0f5de1f10bc51be83a5fb
Vincent Kempers <me@vncn.tk>
2018-07-02 16:51:10 +0200

    add icon clickable

--- extensions/core/interfaces/toggle-icon/interface.vue
+++ extensions/core/interfaces/toggle-icon/interface.vue
@@ -1,20 +1,36 @@
 <template>
-	<div>
-		<i class="material-icons" @click="$emit('input', !value)">{{ value ? 'star' : 'star_border' }}</i>
+	<div v-if="!checkbox">
+		<input
+		type="checkbox"
+		:id="icon-toggle"
+		@click="$emit('input', !value)">
+		<label :for="icon-toggle">
+			<i class="material-icons">{{ icon }}</i>
+		</label>
 	</div>
 </template>
 
 <script>
-import mixin from '../../../mixins/interface';
+import mixin from "../../../mixins/interface";
 
-	export default {
-		mixins: [mixin],
-		computed: {
-
-		}
-	}
+export default {
+  mixins: [mixin],
+  computed: {
+    icon() {
+      return this.value ? "star" : "star_border";
+    }
+  }
+};
 </script>
 
 <style type="scss" scoped>
-
+div {
+  input {
+  }
+  label {
+    i {
+      fill: yellow;
+    }
+  }
+}
 </style>
