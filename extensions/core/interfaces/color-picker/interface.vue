f219221e29cb9741bcbbf3fd3c58d09807db1f8c
Vincent Kempers <me@vncn.tk>
2018-07-16 15:53:47 +0200

    add label click things

---
+++ extensions/core/interfaces/color-picker/interface.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="interface-color-picker">
+        <form action="">
+            <label>
+                <input type="radio"> 
+                    color name
+            </label>
+            <label for="color-picker">
+                <input type="radio"> color name
+            </label>
+            <label for="color-picker">
+                <input type="radio"> color name
+            </label>
+            <label for="color-picker">
+                <input type="radio"> color name
+            </label>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    log() {}
+  }
+};
+</script>
+
+<style type="scss" scoped>
+.color-400 {
+  width: 2em;
+  height: 2em;
+  background-color: green;
+}
+.color-400:hover {
+  width: 3em;
+  height: 3em;
+  transition: all 0.3s ease-in;
+}
+</style>
