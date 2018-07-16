93cfbf43c769e5c3cdd9539417e370c0c918c5be
Vincent Kempers <me@vncn.tk>
2018-07-16 17:50:07 +0200

    add color-picker interface

--- extensions/core/interfaces/color-picker/interface.vue
+++ extensions/core/interfaces/color-picker/interface.vue
@@ -1,40 +1,59 @@
 <template>
     <div class="interface-color-picker">
-        <form action="">
-            <label>
-                <input type="radio"> 
-                    color name
-            </label>
-            <label for="color-picker">
-                <input type="radio"> color name
-            </label>
-            <label for="color-picker">
-                <input type="radio"> color name
-            </label>
-            <label for="color-picker">
-                <input type="radio"> color name
-            </label>
-        </form>
+      <button
+        v-for="color in colors"
+        v-tooltip="$helpers.formatTitle(color)"
+        :key="color"
+        :style="{ backgroundColor: `var(--${color})`}"
+        :class="{ active: value === color}"
+        @click="$emit('input', color)">
+        <template 
+          v-if="value === color">
+          <i class="material-icons">check</i>
+        </template>
+      </button>
     </div>
 </template>
 
 <script>
+import mixin from "../../../mixins/interface";
+import colors from "./colors.json";
+
 export default {
+  mixins: [mixin],
   computed: {
-    log() {}
+    colors() {
+      return colors;
+    }
   }
 };
 </script>
 
-<style type="scss" scoped>
-.color-400 {
-  width: 2em;
-  height: 2em;
-  background-color: green;
-}
-.color-400:hover {
-  width: 3em;
-  height: 3em;
-  transition: all 0.3s ease-in;
+<style lang="scss" scoped>
+.interface-color-picker {
+  --swatch-size: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  width: calc(11 * var(--swatch-size));
+
+  button {
+    flex-basis: var(--swatch-size);
+    width: var(--swatch-size);
+    height: var(--swatch-size);
+    display: block;
+    transform: scale(1);
+    transition: transform var(--fast) var(--transition-out);
+    &:hover {
+      transform: scale(2);
+      z-index: +1;
+      transition: transform var(--fast) var(--transition-in);
+    }
+    i {
+      font-size: 14px;
+      color: hsl(0, 0%, 100%);
+      filter: saturate(0);
+      mix-blend-mode: difference;
+    }
+  }
 }
 </style>
--- extensions/core/interfaces/color-picker/readonly.vue
+++ extensions/core/interfaces/color-picker/readonly.vue
@@ -1,11 +1,26 @@
 <template>
-	<div>
-
+	<div 
+	:style="{ backgroundColor: `var(--${value})`}"
+	v-tooltip="tooltipValue">
 	</div>
 </template>
 
 <script>
+import mixin from "../../../mixins/interface";
+export default {
+  mixins: [mixin],
+  computed: {
+    tooltipValue() {
+      return this.value ? this.$helpers.formatTitle(this.value) : null;
+    }
+  }
+};
 </script>
 
-<style type="scss" scoped>
+<style lang="scss" scoped>
+div {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+}
 </style>
